df2c6b53e71413668d1bcda1ee34b5ba32e6ccf0
BiswanathBD <biswanath.sarker.bd@gmail.com>
2025-08-29 22:09:40 +0600

    mobile menu set

--- index.js
+++ index.js
@@ -12,20 +12,34 @@ console.log(productCards);
 Array.from(productCards).forEach((productCard) => {
   productCard.classList.add(
     "bg-white",
-    'p-4',
+    "p-4",
     "flex",
     "flex-col",
     "text-center",
     "rounded-xl"
   );
-  productCard
-    .querySelector("img")
-    .classList.add("w-[50%]","p-4", "mb-4", "rounded-md", "mx-auto");
-  productCard.querySelector(".star").classList.add("text-yellow-500");
-  productCard.querySelector('.image').classList.add('bg-gray-100', "mb-8")
-  productCard
-  .querySelector(".product-title")
-  .classList.add("text-xl", "font-semibold");
-  productCard.querySelector(".products-price").classList.add("text-gray-500");
 
+  productCard.querySelector("img").classList.add("w-[50%]");
+
+  productCard.querySelector(".star").classList.add("text-yellow-500");
+
+  productCard
+    .querySelector(".image")
+    .classList.add("bg-gray-100", "mb-8", "rounded-xl");
+
+  productCard
+    .querySelector(".product-title")
+    .classList.add("text-xl", "font-semibold");
+
+  productCard.querySelector(".products-price").classList.add("text-gray-500");
+});
+
+// mobile menu function
+const menuBtn = getById("menu-btn");
+menuBtn.addEventListener("click", function () {
+  const mobileMenu = getById("mobile-menu");
+  mobileMenu.classList.toggle("opacity-0");
+  mobileMenu.classList.toggle("opacity-100");
+  mobileMenu.classList.toggle("top-10");
+  mobileMenu.classList.toggle("top-16");
 });
